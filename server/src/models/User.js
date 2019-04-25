7b9c8127e2c99dfea0e3cec0f536c9955e1643b8
aqibero <arnold.ibero@gmail.com>
2019-04-25 21:12:34 +0800

    Passwordhash issue fixed

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,38 +1,16 @@
-const Promise = require('bluebird')
-const bcrypt = Promise.promisifyAll(require('bcrypt-nodejs'))
-
-function hashPassword (user, options){
-  const SALT_FACTOR = 8
-  console.log('hashPassword')
-  
-  if (!user.changed('password')) {
-    return;
-  }
-  return bcrypt
-    .genSaltSync(SALT_FACTOR)
-    .then(salt => bycrypt.hashAsync(user.password, salt, null))
-    .then(hash => {
-      user.setDataValue('password', hash)
-    })
-}
+const bcrypt = require('bcrypt')
 
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     email: {
-        type: DataTypes.STRING,
-        unique: true
+      type: DataTypes.STRING,
+      unique: true
     },
     password: DataTypes.STRING
-  }, {
-    hooks: {
-      // beforeCreate: hashPassword,
-      // beforeUpdate: hashPassword,
-      beforeSave: hashPassword
-    }
   })
 
-  User.prototype.comparePassword = function (password){
-    return bcrypt.compareAsync(password, this.password)
+  User.prototype.comparePassword = function (password) {
+    return bcrypt.compare(password, this.password)
   }
 
   return User
--- server/src/policies/AuthenticationControllerPolicy.js
+++ server/src/policies/AuthenticationControllerPolicy.js
@@ -1,40 +1,72 @@
-const Joi = require('joi')
+const { User } = require('../models')
+const jwt = require('jsonwebtoken')
+const config = require('../config/config')
+const bcrypt = require('bcrypt')
+
+function jwtSignUser (user) {
+  const ONE_WEEK = 60 * 60 * 24 * 7
+  return jwt.sign(user, config.authentication.jwtSecret, {
+    expiresIn: ONE_WEEK
+  })
+}
+
+async function hashPassword (pwd) {
+  const SALT_FACTOR = 12
+  const result = await bcrypt.hash(pwd, SALT_FACTOR)
+  .then(function (hash) {
+    return hash
+  })
+  return result
+}
 
 module.exports = {
-  register (req, res, next) {
-    const schema = {
-      email:Joi.string().email(),
-      password: Joi.string().regex(
-        new RegExp('^[a-zA-Z0-9]{8,32}$')
-      )
-    }
-
-const {error, value} = Joi.validate(req.body, schema)
-
-if (error) {
-  switch (error.details[0].context.key) {
-    case 'email':
-      res.status(400).send({
-        error: 'You must provide a valid email address'
+  async register (req, res) {
+    try {
+      req.body.password = await hashPassword(req.body.password)
+      const user = await User.create(req.body)
+      const userJson = user.toJSON()
+      res.send({
+        user: userJson,
+        token: jwtSignUser(userJson)
       })
-      break
-    case 'password':
+    } catch (error) {
+      // e-mail already exists or such
       res.status(400).send({
-        error: `The password provided failed to match the following rules:
-          <br>
-          1. It must contain ONLY the following characters: lower case, UPPER CASE, and numerics.
-          <br>
-          2. It must be at least 8 characters in lenght and not grater than 32 characters in leght.
-          `
-        })
-        break
-      default:
-        res.status(400).send({
-          error: 'Invalid registration information'
+        error: 'This email address is already in use'
+      })
+    }
+  },
+  async login (req, res) {
+    try {
+      // Grab user input
+      const { email, password } = req.body
+      const user = await User.findOne({
+        where: {
+          email: email
+        }
+      })
+      // Check to see if user is in db
+      if (!user) {
+        res.status(403).send({
+          error: 'the login information was incorrect / Not Found'
         })
       }
-    } else {
-    next()
+      // Check to see if password is valid
+      const isPasswordValid = await user.comparePassword(password)
+      if (!isPasswordValid) {
+        return res.status(403).send({
+          error: 'The login information was incorrect'
+        })
+      }
+      // return user using toJSON()
+      const userJson = user.toJSON()
+      res.send({
+        user: userJson,
+        token: jwtSignUser(userJson)
+      })
+    } catch (e) {
+      res.status(500).send({ error: 'An error occured attempting to login' })
+      console.log(e)
     }
   }
 }
