2f48220b48c1b5317b6021fdcca4976a7bac5641
aqibero <arnold.ibero@gmail.com>
2019-04-25 20:54:53 +0800

    Added user token

--- server/src/controllers/AuthenticationController.js
+++ server/src/controllers/AuthenticationController.js
@@ -1,4 +1,13 @@
 const {User} = require('../models')
+const jwt = require('jsonwebtoken')
+const config = require('../config/config')
+
+function jwtSignUser (user){
+  const ONE_WEEK = 60 * 60 * 24 * 7
+  return jwt.sign(user, config.authentication.jwtSecret, {
+    expiresIn: ONE_WEEK
+  })
+}
 
 module.exports = {
   async register (req, res) {
@@ -7,7 +16,7 @@ module.exports = {
         res.send(user.toJSON())
       }
       catch (error){
-        res.status(400).send({
+        res.status(401).send({
           error: 'Email address is already in used.'
         })
       } 
@@ -20,19 +29,25 @@ module.exports = {
             email: email
           }
         })
+        // console.log('user', user.toJSON())
         if (!user){
           return res.status(403).send({
             error: 'The login information was incorrect'
           })
         }
-        const isPasswordValid = password === user.password
+        const isPasswordValid = await user.comparePassword(password)
+        // console.log(password, user.password)
+        // console.log('password', isPasswordValid)
         if (!isPasswordValid) {
           return res.status(403).send({
             error: ' The login information was incorrect'
           })
         }
-        const userJson = user.toJSON
-        res.send(userJson)
+        const userJson = user.toJSON()
+        res.send({
+          user: userJson,
+          token: jwtSignUser(userJson)
+        })
       }
       catch (error){
         res.status(500).send({
